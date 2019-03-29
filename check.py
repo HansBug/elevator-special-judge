dc4a563fc64c197d176d087a8dc56682c9ba2820
周雨飞 <zhouyufei1998@vip.qq.com>
2019-03-30 00:14:17 +0800

    use new random model

--- check.py
+++ check.py
@@ -36,14 +36,27 @@ def _check_validity(request_list):
 def _check_time(request_list, need_max=False):
     base_run_timespan = 0.5
     base_serve_timespan = 0.5
+    run_timespan_disturb = 0.05
+    serve_timespan_disturb = 0.05
+    request_time_disturb_upper_bound = 0.1
+    request_time_disturb_lower_bound = -0.1
     max_time = 0.0
     for i in range(500):
-        run_timespan = base_run_timespan + random.uniform(0, +0.1)
-        serve_timespan = base_serve_timespan + random.uniform(0, +0.1)
+        run_timespan = base_run_timespan + random.uniform(0, run_timespan_disturb)
+        serve_timespan = base_serve_timespan + random.uniform(0, serve_timespan_disturb)
         time = 0.0
         floor = 1
-        for request in request_list:
-            request_time = max(0, request['time'] + random.uniform(-0.1, +0.1))
+        last_request_random_time = 0.0
+        for index, request in enumerate(request_list):
+            request_real_time = request['time']
+            request_random_disturb = random.uniform(request_time_disturb_upper_bound, request_time_disturb_lower_bound)
+            if index == 0:
+                request_time = max(0, request_real_time + request_random_disturb)
+            else:
+                request_time = max(
+                    max(last_request_random_time, request_real_time) + request_random_disturb,
+                    last_request_random_time)
+            last_request_random_time = request_time
             request_start_floor = request['start']
             request_end_floor = request['end']
             if request_time > time:
