6e3a74407f148477092bda22c0671c19c649d52f
周雨飞 <zhouyufei1998@vip.qq.com>
2019-04-07 00:39:33 +0800

    fix bug in ALS

--- check.py
+++ check.py
@@ -48,9 +48,9 @@ def __check_validity(request_list):
 
 
 base_run_timespan = 0.4
-base_serve_timespan = 0.5
+base_serve_timespan = 0.4
 run_timespan_disturb = 0.04
-serve_timespan_disturb = 0.05
+serve_timespan_disturb = 0.04
 request_time_disturb_upper_bound = 0.1
 request_time_disturb_lower_bound = -0.1
 basement_floor_count = 3
@@ -90,19 +90,28 @@ def __simulate(request_list, run_timespan, serve_timespan):
         floor_time_checkpoints[_end]['served'] = True
         floor_time_checkpoints[_end]['time'] = base_time + run_timespan * abs(_end - _start) + serve_timespan
 
-    def __update_pickup_time(_start, _end, _main_end):
+    def __update_pickup_time(_start, _end, _travel_end):
+        direction = int(math.copysign(1, _end - _start))
+        base_update_time = floor_time_checkpoints[_travel_end]['time']
+        for i in range(_travel_end, _end + direction, direction):
+            if i != _travel_end:
+                base_update_time += run_timespan
+                floor_time_checkpoints[i]['time'] = base_update_time
         base_update_time = 0.0
         if not floor_time_checkpoints[_start]['served']:
             base_update_time += serve_timespan
             floor_time_checkpoints[_start]['served'] = True
-        for i in range(_start, _end, int(math.copysign(1, _end - _start))):
+        for i in range(_start, _end + direction, direction):
             floor_time_checkpoints[i]['time'] += base_update_time
         if not floor_time_checkpoints[_end]['served']:
             base_update_time += serve_timespan
             floor_time_checkpoints[_end]['served'] = True
-        for i in range(_end, _main_end, int(math.copysign(1, _end - _start))):
+        for i in range(_end, _travel_end + direction, direction):
             floor_time_checkpoints[i]['time'] += base_update_time
-        floor_time_checkpoints[_main_end]['time'] += base_update_time
+        if _start < _end < _travel_end or _travel_end < _end < _start:
+            if not floor_time_checkpoints[_travel_end]['served']:
+                floor_time_checkpoints[_travel_end]['served'] = True
+                floor_time_checkpoints[_travel_end]['time'] += serve_timespan
 
     while not all([request['served'] for request in request_list]):
         floor_time_checkpoints = [{'served': False, 'time': 0.0} for i in range(19)]
@@ -115,6 +124,7 @@ def __simulate(request_list, run_timespan, serve_timespan):
         start = main_request['start']
         end = main_request['end']
         __build_basic_time(time, start, end)
+        travel_end = end
         while True:
             next_pickup_request = next((request for request in request_list
                                         if not request['served'] and
@@ -125,15 +135,17 @@ def __simulate(request_list, run_timespan, serve_timespan):
                                         request['time'] <= floor_time_checkpoints[request['start']]['time']), None)
             if not next_pickup_request:
                 break
-            __update_pickup_time(next_pickup_request['start'], next_pickup_request['end'], end)
+            __update_pickup_time(next_pickup_request['start'], next_pickup_request['end'], travel_end)
+            travel_end = max(travel_end, next_pickup_request['end']) if end > start \
+                else min(travel_end, next_pickup_request['end'])
             pickup_request_bundle.append(next_pickup_request)
         for request in pickup_request_bundle:
             if min(start, end) <= request['start'] < max(start, end):
                 request['served'] = True
         pickup_request_bundle = [request for request in pickup_request_bundle if not request['served']]
         main_request['served'] = True
-        last_request_finish_time = floor_time_checkpoints[main_request['end']]['time']
-        floor = main_request['end']
+        last_request_finish_time = floor_time_checkpoints[travel_end]['time']
+        floor = travel_end
 
     time = last_request_finish_time
     return time
